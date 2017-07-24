ffb9985ed519c12532dca6173b5a612da8ed6dea
Ian <laurent.roque@yahoo.com>
2017-07-24 13:34:12 -0700

    first commit for "superscript.py"

---
+++ event_builder/superscript.py
@@ -0,0 +1,55 @@
+#Distributes data over cores and displays the first 5 entries of each core
+
+#logistical support
+from mpi4py import MPI
+import h5py, glob, time, sys
+import numpy as np
+
+#initializatoin commands
+nbatch = int(sys.argv[1])
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size() #number of cores
+#offset = rank*
+
+#profile mpitime
+comm.Barrier()
+start = MPI.Wtime()
+
+#list distribution for file 1
+time1Dist = h5py.File('file1.h5', 'r')
+times=time1Dist['timestamp1']
+time2Dist = h5py.File('file2.h5', 'r')
+
+#check valid nbatch
+if nbatch > len(times)/size: 
+  print "Batch size is too large. Maximum size is", len(times)/size
+  exit()
+
+#striping
+mytime = [i for i in xrange(len(times)) if i%(size*nbatch) >= (rank*nbatch) and i%(size*nbatch) < (rank+1)*nbatch]
+
+ts = time1Dist['timestamp1'][mytime]
+smldata = time1Dist['smalldata'][mytime]
+
+ds = time1Dist['bigdata1']
+cnEvents = 0
+for i in mytime:
+  evti = ds[i]
+  cnEvents += 1
+
+#for debugging
+print 'TIMESTAMP RANK TSB1[0] TSSIZE NEVENTS: ', rank, ts[0], len(ts), len(mytime)
+
+foundind = np.searchsorted(time2Dist['timestamp2'], ts, side='left')
+for i,ind in enumerate(foundind):
+    if ind == len(time2Dist['timestamp2']): continue
+    if ts[i] == time2Dist['timestamp2'][ind]:
+      dat1 = time1Dist['bigdata1'][i]
+      dat2 = time2Dist['bigdata2'][ind]
+
+comm.Barrier()
+end = MPI.Wtime()
+if rank== 0: print 'NBATCH', nbatch, 'NEXPEVTS_PERRANK', len(times)/size, 'TOTALTIME (s)', end-start
+
+MPI.Finalize()
