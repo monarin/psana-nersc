86a9a119ef5b4ecce116dcc8e9288fa41c79e048
Ian <laurent.roque@yahoo.com>
2017-08-10 18:08:58 -0700

    dealer master now sends list instead of pointer

--- event_builder/mpi_dealer.py
+++ event_builder/mpi_dealer.py
@@ -6,20 +6,17 @@ Execution commands:
     @NERSC => sbatch -o log.txt submit_simple.sh
 '''
 
-#Logistical Support
 from psana import *
 from mpi4py import MPI
 import h5py, sys, time
 import numpy as np
 
-#Initialization Definitions
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 assert size>1
 nbatch = int(sys.argv[1])
 
-#Start of Analysis -- Opening h5 Files
 comm.Barrier()
 start = MPI.Wtime()
 file1 = h5py.File('file1.h5', 'r')
@@ -28,29 +25,27 @@ file2 = h5py.File('file2.h5', 'r')
 #Block distribution among cores
 def master(indices):
   for i in indices:
+    #asking for a free rank
     rankreq = comm.recv(source=MPI.ANY_SOURCE)
-    comm.send(i, dest=rankreq)
+    #hand out a list of indices to that rank
+    if i+nbatch < len(file1['timestamp1']):
+      myIndices=range(i, i+nbatch)
+    else:
+      myIndices = range(i, len(file1['timestamp1']))
+    smlData = file1['smalldata'][myIndices]
+    comm.send(myIndices, dest=rankreq)
   for rankreq in range(size-1):
     rankreq = comm.recv(source=MPI.ANY_SOURCE)
     comm.send('endrun', dest=rankreq)
 
 def client():
   start_local = time.time()
-  myIndices = []
   while True:
     comm.send(rank, dest=0)
-    evtIndex = comm.recv(source=0)
-    if str(evtIndex) == 'endrun': 
+    myIndices = comm.recv(source=0)
+    if str(myIndices) == 'endrun': 
       print "Rank: %d Time Elapsed (s): %6.3f"%(rank, time.time()-start_local)
       break
-#Analysis by blocks of events    
-    if evtIndex+nbatch < len(file1['timestamp1']):
-      myIndices=range(evtIndex, evtIndex+nbatch)
-    else:
-      myIndices = range(evtIndex, len(file1['timestamp1']))
-    # Read smlData
-    smlData = file1['smalldata'][myIndices]
-    # Read bigData in batch mode
     bigData1 = file1['bigdata1'][myIndices]
 #Reading the data one-by-one (original way of reading data)
 #    if len(myIndices) == nbatch:
