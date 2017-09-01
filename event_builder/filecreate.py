3eceda64437d0ac837934b5f472f4e372dd944fe
Ian <laurent.roque@yahoo.com>
2017-09-01 16:48:24 -0700

    close to complete set of event builder scripts

--- event_builder/filecreate.py
+++ event_builder/filecreate.py
@@ -1,102 +1,108 @@
 '''
-NEW AND IMPROVED FILECREATE!!!!
-Creates multiple h5 files each with unique variable length timestamps.
-
-Input the file size you want approximately in GB into 'file1size' variable
-Input the number of h5 files you want into 'number_of_files' variable
-
-CODE MAY NEED CLEANUP IN ORDER TO BE MORE PROFESSIONAL (i.e. using data.resize
-    for the subsequential h5 file loop instead of appending a list)
+New Filecreate file.
+timestamp length will now match big data length
+big data array will be filled with integers instead of zeros
 '''
-
-#Logistical Support
+#logistical support
 import h5py, random
 import numpy as np
 
-#INPUT YOUR OPTIONS HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-file1size = 2 #Size of File 1 in GB (aproximately ... sort of)
-number_of_files = 10 #Total number of h5 files you want
+#Options imported here
+#This is the filesize of the first h5 file. For small kB-size files set to 0
+filesize = 8
+number_of_files = 10
 
-#Initialization variables
-startnum = 0
-r = 2 
+#initial variables
+initialindex = 0
+startonfilenumber = 2
 tsFactor = 1
-if file1size == 0:
-    datSize = 150
+#evetn arrays are written in chunks of this number. DO NOT INSERT ODD NUMBERS HERE!
+chunkSize = 4
+
+#a filesize of 0 will create a small set of h5 files
+if filesize == 0:
+    datSize = 100
 else:
-    datSize = file1size*20000
-datAmount = int(round(datSize/6))
+    datSize = filesize*10000
+    
+#each event will be a megabyte in size
+evtSize = 250000
+
+#small data will be an array of random colors. "red" will be deemed as an interesting event.
 smallDatColors = ['red', 'blue', 'green']
 
-if datSize < 100000:
-    max_block_size = int(round(datSize*.03))
-    bunch = int(round(datSize/50))
+#introduces a sequential block to prevent large arrays of sequential timestamps
+if datSize == 100:
+    max_block_size = int(round(datSize*.05))
+    bunch = int(round(datSize/5))
 else:
-    max_block_size= int(round(datSize*.003))
-    bunch = int(round(datSize/300))
-print 'MAXIMUM TIMESTAMP2 BLOCK SIZE:', max_block_size, 'BUNCH:', bunch
+    max_block_size = int(round(datSize*.005))
+    bunch = int(round(datSize/100))
 
-chunk = np.array([range(datAmount) for i in range(10)])
-row_count = chunk.shape[0]
+#imageing device prevents memory issues when writing large arrays
+image = np.array([range(evtSize) for i in range(chunkSize)])
 
-#Phase I --Creating the first h5 file--
+#creating the first h5 file
 with h5py.File('file1.h5', 'w') as f:
-    #Creating Small Data
-    smallDat1 = f.create_dataset('smalldata', (datSize,), dtype='S5')
+    #creating small data
+    smallDat = f.create_dataset('smalldata', (datSize,), dtype='S5')
     for event in range(datSize):
-        smallDat1[event] = (random.choice(smallDatColors))
-    print 'SMALLDATA1:', smallDat1[:]
-
-    #Creating first Timestamp array
+        smallDat[event] = (random.choice(smallDatColors))
+    
+    #creating first timestamp array
     firstStamp = f.create_dataset('timestamp1', (datSize,), dtype='i')
     for stamp in range(datSize):
-        firstStamp[stamp] = startnum+(stamp*tsFactor)
-    print 'TIMESTAMP1:', firstStamp[:]
+        firstStamp[stamp] = initialindex+(stamp*tsFactor)
     
-    #Creating first Event array
-    maxshape = (None,) + chunk.shape[1:]
-    bigDat1 = f.create_dataset('bigdata1', shape=chunk.shape,
-      maxshape=maxshape, chunks=chunk.shape, dtype=chunk.dtype)
-    bigDat1[:] = chunk
-    for i in range(int(datSize/chunk.shape[0])-1):
-        bigDat1.resize(row_count + chunk.shape[0], axis=0)
-        bigDat1[row_count:] = chunk
-        row_count += chunk.shape[0]
-    print 'BIGDATA1:', bigDat1[:]
-
-#Phase II --Creating subsequent h5 files--
-#Crappy way of getting the time stamps to work out below...
-#Please fix with datset.resize as done with File1's bigDat1
-while r <= number_of_files:
-  list = np.arange(datSize)[...]
-  amendment = [] 
-  while startnum < len(list):   
-    sel_n = int(round(random.choice(range(startnum,startnum+bunch))))
-    block_size = random.choice(range(max_block_size))
-    if sel_n+block_size > len(list):
-        block_size = len(list)- sel_n
-    for i in range(sel_n, sel_n+block_size):
-        amendment.append(list[i])
-    startnum = sel_n+block_size
+    #creating first event array
+    maxshape = (None,) + image.shape[1:]
+    print datSize, image.shape
+    bigDat1 = f.create_dataset('bigdata1', shape=image.shape,
+      maxshape=maxshape, chunks=image.shape, dtype=image.dtype)
+    bigDat1[:] = image
+    row_count = chunkSize
+    for i in range(int(datSize/image.shape[0])-1):
+        bigDat1.resize(row_count + image.shape[0], axis =0)
+        bigDat1[row_count:] = image
+        row_count += image.shape[0]
     
-  with h5py.File('file%s.h5' %r, 'w') as g:
-      #Creating Timestamp arrays
-      secondStamp = g.create_dataset('timestamp%s' %r, (len(amendment),), dtype='i')    
-      length = len(amendment)
-      secondStamp[:length]=amendment[:]
-      print 'TIMESTAMP', r, ':', secondStamp[:]
-    
-      #Creating Event Data arrays
-      maxshape = (None,) + chunk.shape[1:]
-      bigDat2 = g.create_dataset('bigdata%s' %r, shape=chunk.shape, maxshape=maxshape,
-        chunks=chunk.shape, dtype=chunk.dtype)
-      bigDat2[:] = chunk
-      for i in range(int(len(amendment)/chunk.shape[0])-1):
-        bigDat2.resize(row_count + chunk.shape[0], axis=0)
-        bigDat2[row_count:] = chunk
-        row_count += chunk.shape[0]
-      print 'BIGDATA', r, ':', bigDat2[:]
-
-  #Part of the loop
-  r = r+1
-  startnum = 0
+#creating the secondary h5 files
+while startonfilenumber <= number_of_files:
+    list = np.arange(datSize)[...]
+    amendment = []
+    while initialindex < len(list):
+        sel_n = int(round(random.choice(range(initialindex, initialindex+bunch))))
+        block_size = random.choice(range(max_block_size))
+        if sel_n+block_size > len(list):
+            block_size = len(list)-sel_n
+        for i in range(sel_n, sel_n+block_size):
+            amendment.append(list[i])
+        initialindex = sel_n+block_size
+        
+    with h5py.File('file%s.h5' %startonfilenumber, 'w') as g:
+        #creating timestamp arrays
+        secondStamp = g.create_dataset('timestamp%s' %startonfilenumber, (len(amendment),), dtype='i')
+        length = len(amendment)
+        secondStamp[:length]=amendment[:]
+        import math
+        nChunks = int(math.floor(len(secondStamp)/chunkSize))
+        nOffsets = len(secondStamp)%chunkSize
+        
+        #creating event data arrays
+        maxshape = (None,) + image.shape[1:]
+        bigDat = g.create_dataset('bigdata%s' %startonfilenumber, shape=image.shape,
+          maxshape=maxshape, chunks=image.shape, dtype=image.dtype)
+        bigDat[:] = image
+        #create chunks
+        row_count = chunkSize
+        for i in range(nChunks-1):
+          bigDat.resize(row_count + image.shape[0], axis =0)
+          bigDat[row_count:] = image
+          row_count += image.shape[0]
+        #create offsets of the last chunk
+        for i in range(nOffsets):
+          bigDat.resize(row_count + 1, axis=0)
+          bigDat[row_count] = np.arange(0, evtSize)
+          row_count += 1
+    startonfilenumber += 1
+    initialindex = 0
