016a0f8417732c8014f8909491fd4ae247e66730
monarin <monarin@gmail.com>
2018-07-10 19:17:54 -0700

    Lastest docker version and submit script.

--- ez_slurm/auto/submit_single.sh
+++ ez_slurm/auto/submit_single.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -l
-#SBATCH --partition=debug
 #SBATCH --account=lcls
 #SBATCH --job-name=psauto
 #SBATCH --nodes=3
 #SBATCH --constraint=knl,quad,cache
-#SBATCH --time=00:30:00
-#SBATCH --image=docker:monarin/psanatest:latest
+#SBATCH --time=00:10:00
+#SBATCH --image=docker:monarin/psananersc:latest
+#SBATCH --qos=premium
+
 t_start=`date +%s`
 srun -n 137 -c 4 --cpu_bind=cores shifter /global/cscratch1/sd/monarin/d/psdm/cxi/cxid9114/scratch/mona/psana-nersc/ez_slurm/auto/index.sh cxid9114 96 96 2 lustre None debug 
 t_end=`date +%s`
