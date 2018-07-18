0aff72103fc8a875c4b3b3be141eb73d284f0fb5
monarin <monarin@gmail.com>
2018-07-18 16:53:14 -0700

    Make experiment constants to variables.

--- demo18/submit.cmd
+++ demo18/submit.cmd
@@ -6,6 +6,12 @@
 #BSUB -e cctbx_error.%J.log     # error file name in which %J is replaced by the job ID
 #BSUB -o cctbx_output.%J.log     # output file name in which %J is replaced by the job ID
 
+# experiment parameters
+EXP="cxid9114"
+RUN=96
+TRIAL=0
+RUN_F="$(printf "r%04d" ${RUN})"
+TRIAL_F="$(printf "%03d" ${TRIAL})"
 
 export IN_DIR=$PWD/input
 export LIMIT=10
@@ -17,19 +23,20 @@ source ~/lcls2/setup_env_python2.sh
 conda activate ps2cctbx
 source /reg/neh/home/monarin/.conda/envs/ps2cctbx/cctbx/build/setpaths.sh
 
-mkdir -p $OUT_DIR/discovery/dials/r0108/000/out
-mkdir -p $OUT_DIR/discovery/dials/r0108/000/stdout
-mkdir -p $OUT_DIR/discovery/dials/r0108/000/tmp
+# setup playground
+mkdir -p ${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/out
+mkdir -p ${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/stdout
+mkdir -p ${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/tmp
 
 mpirun cctbx.xfel.xtc_process \
-   input.experiment=cxid9114 \
-   input.run_num=108 \
-   output.logging_dir=$OUT_DIR/discovery/dials/r0108/000/stdout \
-   output.output_dir=$OUT_DIR/discovery/dials/r0108/000/out \
+   input.experiment=${EXP} \
+   input.run_num=${RUN} \
+   output.logging_dir=${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/stdout \
+   output.output_dir=${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/out \
    format.cbf.invalid_pixel_mask=$IN_DIR/mask_ld91.pickle \
    $IN_DIR/process_batch.phil \
    dump_indexed=False \
-   output.tmp_output_dir=$OUT_DIR/discovery/dials/r0108/000/tmp \
-   input.reference_geometry=$IN_DIR/geom_ld91.json \
+   output.tmp_output_dir=${OUT_DIR}/discovery/dials/${RUN_F}/${TRIAL_F}/tmp \
+   input.reference_geometry=${IN_DIR}/geom_ld91.json \
    input.xtc_dir=$DATA_DIR \
    max_events=$LIMIT
