b76d77ddbd41c520804b22b88ea60d2c9b4cca39
monarin <monarin@gmail.com>
2018-10-15 17:27:11 -0700

    Latest success job script after adding photonEnergy.

--- demo18/cxic0415/submit_single.sh
+++ demo18/cxic0415/submit_single.sh
@@ -3,15 +3,16 @@
 #SBATCH --job-name=ps2cctbx
 #SBATCH --nodes=1
 #SBATCH --constraint=knl,quad,cache
-#SBATCH --time=00:15:00
-#SBATCH --image=docker:monarin/ps2cctbx:latest
+#SBATCH --time=01:00:00
+#SBATCH --image=docker:monarin/ps2cctbx:test
+#SBATCH --exclusive
 #SBATCH --qos=premium
 
 t_start=`date +%s`
 
 export PMI_MMAP_SYNC_WAIT_TIME=600
+MAX_EVENTS=66
 sbcast -p ./input/process_batch.phil /tmp/process_batch.phil
-sbcast -p ./xtc_process.py /tmp/xtc_process.py
-srun -n 68 -c 4 --cpu_bind=cores shifter ./index_single.sh cxic0415 1 0 debug 64 /tmp
+srun -n 68 -c 4 --cpu_bind=cores shifter ./index_single.sh cxic0415 50 0 none $MAX_EVENTS ${PWD}/output
 t_end=`date +%s`
 echo PSJobCompleted TotalElapsed $((t_end-t_start)) $t_start $t_end
