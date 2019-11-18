53984a14ddd85a558da7caf0da0bec1db6c043ff
Monarin Uervirojnangkoorn <monarin@gmail.com>
2019-11-18 14:11:41 -0500

    Update psana2 test files for SUMMIT. (more)

---
+++ psana2/submit_summit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#BSUB -P CHM137
+#BSUB -W 00:10
+#BSUB -nnodes 21
+#BSUB -alloc_flags gpumps
+#BSUB -J RunPsana2
+#BSUB -o RunPsana2.%J
+#BSUB -e RunPsana2.%J
+ 
+t_start=`date +%s`
+
+source $MEMBERWORK/chm137/adse13_161/summit/env.sh
+
+#jsrun -n44 -r11 -a1 -c1 -g0 ./run.sh
+jsrun --erf_input erf.txt ./run.sh
+
+t_end=`date +%s`
+echo PSJobCompleted TotalElapsed $((t_end-t_start)) $t_start $t_end
