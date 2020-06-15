broadcast timestamp to core0 Fix conflicts

psana2/dev_bd.py
@@ -10,16 +10,17 @@ rank = comm.Get_rank()
 
 #xtc_dir = "/gpfs/alpine/proj-shared/chm137/data/LD91"
 #xtc_dir = "/gpfs/alpine/proj-shared/chm137/data/test/.tmp"
-xtc_dir = "/ffb01/mona/xtc2/.tmp"
-batch_size = 1 
-max_events = 100000 
+#xtc_dir = "/ffb01/mona/xtc2/.tmp"
+xtc_dir = "/reg/neh/home/monarin/tmp/.tmp"
+batch_size = 1000
+max_events = 0
 
 def filter_fn(evt):
     return True
 
 # Usecase 1a : two iterators with filter function
 st = MPI.Wtime()
-ds = DataSource(exp='xpptut15', run=1, dir=xtc_dir, batch_size=batch_size, max_events=max_events, filter=filter_fn)
+ds = DataSource(exp='xpptut13', run=1, dir=xtc_dir, batch_size=batch_size, max_events=max_events, filter=filter_fn)
 
 ds_done_t = MPI.Wtime()
 
@@ -32,6 +33,7 @@ recvbuf = None
 if rank == 0:
     recvbuf = np.empty([size, 1], dtype='i')
 
+sendstr = ''
 for run in ds.runs():
     #det = run.Detector('cspad')
     det = run.Detector('xppcspad')
@@ -41,13 +43,17 @@ for run in ds.runs():
         #raw = det.raw.raw(evt)
         raw = det.raw.calib(evt)
         #print(evt.timestamp, raw.shape)
+        sendstr += f'{rank} {time.time()}\n'
 
 run_done_t = MPI.Wtime()
 
 comm.Gather(sendbuf, recvbuf, root=0)
+sendstr = comm.gather(sendstr, root=0)
 en = MPI.Wtime()
 
 if rank == 0:
     n_events = np.sum(recvbuf)
     evtbuilder = int(os.environ.get('PS_SMD_NODES', 1))
-    print(f'#eb: {evtbuilder} #evt:{n_events} total(s): {en-st:.2f} rate(kHz): {n_events/((en-st)*1000):.2f} ds(s): {ds_done_t-st:.2f} barrier(s): {barrier_t-ds_done_t:.2f} run(s): {run_done_t-barrier_t:.2f} gather(s): {en-run_done_t:.2f} ds_called: {ds_called_ts:.0f}')
+    with open('log_dev_bd.txt', 'w') as f:
+        f.write(''.join(sendstr))
+        f.write(f'#eb: {evtbuilder} #evt:{n_events} total(s): {en-st:.2f} rate(kHz): {n_events/((en-st)*1000):.2f} ds(s): {ds_done_t-st:.2f} barrier(s): {barrier_t-ds_done_t:.2f} run(s): {run_done_t-barrier_t:.2f} gather(s): {en-run_done_t:.2f} ds_called: {ds_called_ts:.0f}')

psana2/dev_smd0.py
@@ -7,12 +7,12 @@ from psana.dgram import Dgram
 import numpy as np
 
 chunksize = 0x1000000
-max_events = 10000000
-smd0_batch_size = 13500*16
+max_events = 100
+smd0_batch_size = 1
 
 def run_smd0():
-    #filenames = glob.glob('/reg/neh/home/monarin/psana-nersc/psana2/.tmp/smalldata/*.xtc2')
-    filenames = glob.glob('/ffb01/mona/.tmp/smalldata/*.xtc2')
+    filenames = glob.glob('/reg/neh/home/monarin/psana-nersc/psana2/.tmp/smalldata/*.xtc2')
+    #filenames = glob.glob('/ffb01/mona/.tmp/smalldata/*.xtc2')
 
     fds = np.array([os.open(filename, os.O_RDONLY) for filename in filenames], dtype=np.int32)
 
@@ -38,21 +38,24 @@ def run_smd0():
 
     smdr.get(how_many)
     while smdr.got_events > 0:
-        for i in range(limit):
-            view = smdr.view(i)
-            """
-            if view:
-                cn_dgrams = 0
-                while offsets[i] < view.shape[0]:
-                    d = Dgram(config=configs[i], view=view, offset=offsets[i])
-                    print(f' buf{i} d_id: {cn_dgrams} d_ts {d.timestamp() & 0xffffffff}')
-                    offsets[i] += d._size
-                    cn_dgrams += 1
-                #print(f'smdr_man got {memoryview(view).nbytes}')
-            else:
-                #print(f' buf[{i} empty')
-                pass
-            """
+#        for i in range(limit):
+#            view = smdr.view(i)
+#            """
+#            if view:
+#                cn_dgrams = 0
+#                while offsets[i] < view.shape[0]:
+#                    d = Dgram(config=configs[i], view=view, offset=offsets[i])
+#                    print(f' buf{i} d_id: {cn_dgrams} d_ts {d.timestamp() & 0xffffffff}')
+#                    offsets[i] += d._size
+#                    cn_dgrams += 1
+#                #print(f'smdr_man got {memoryview(view).nbytes}')
+#            else:
+#                #print(f' buf[{i} empty')
+#                pass
+#            """
+#
+#
+#
         processed_events += smdr.got_events
         if processed_events >= max_events:
             break