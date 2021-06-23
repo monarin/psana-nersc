a6e869a76c4e963bef16de3fb41791a17e283102
monarin <monarin@gmail.com>
2021-06-23 16:46:19 -0700

    simple script to count no. of hosts

---
+++ psana2/count_hosts.py
@@ -0,0 +1,18 @@
+import sys, os
+filename = sys.argv[1]
+hosts={}
+cn_hosts = 0
+cn_ranks = 0
+with open(filename, 'r') as f:
+    for line in f:
+        cols = line.split()
+        hostname = cols[8]
+        if hostname in hosts:
+            hosts[hostname] += 1
+        else:   
+            hosts[hostname] = 1
+            cn_hosts += 1
+        cn_ranks += 1
+
+print(f'#hosts: {len(hosts.keys())} {cn_hosts} {cn_ranks}')
+print(hosts)
