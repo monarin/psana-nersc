0e29f71c40bee94a6df70062b548964f2ab7a926
monarin <monarin@gmail.com>
2022-02-28 11:49:27 -0800

    Add example to convert raw to fex quaanode data

---
+++ psana2/dgrampy/ex-01-conv-raw-to-fex-wf.py
@@ -0,0 +1,200 @@
+""" Converts raw to fex-style waveforms
+
+    Note: 
+        modified from lcls2/psana/psana/hexanode/examples/
+        ex-22-data-acqiris-peaks-save-h5-xiangli.py
+    Input raw waveforms:
+        time 1 2 3 4 5 6 7 8 9....
+        wf = 2 3 4 5 4 3 2 1 1 1 2 3 4 6 3 2 1 1 1
+    threshold:
+        3
+    Output fex peaks:
+        [3,4,5,4,3] starttime 2
+        [3,4,6,3] starttime 12
+"""
+import logging
+logger = logging.getLogger(__name__)
+
+import sys
+from time import time
+
+from psana import DataSource
+from psana.hexanode.WFPeaks import WFPeaks
+
+from psana.pyalgos.generic.NDArrUtils import print_ndarr
+from psana.pyalgos.generic.Utils import str_kwargs, do_print
+
+import numpy as np
+
+USAGE = 'Usage: python %s' % sys.argv[0]
+
+def get_window_from_peaks(wf, wt, peak_ind, window_size=8):
+    """Returns a list of 1D waveforms and start positions.
+    
+    From each peak index in `peak_ind`, copy waveform per channel
+    wf[i_peak, -window_size+1 to window_size] 
+    to a new array. 
+    """
+    n_peaks = len(peak_ind)
+    peaks_arr = np.zeros([n_peaks, window_size*2], dtype=wf.dtype)
+    startpos_arr = np.zeros(n_peaks, dtype=wt.dtype)
+    for i_peak, pkind in enumerate(peak_ind):
+        peaks_arr[i_peak,:] = wf[pkind-window_size+1: pkind+window_size+1]
+        startpos_arr[i_peak] = wt[pkind-window_size+1]
+    return peaks_arr, startpos_arr
+
+def proc_data(**kwargs):
+
+    logger.info(str_kwargs(kwargs, title='Input parameters:'))
+
+    DSNAME       = kwargs.get('dsname', '/reg/g/psdm/detector/data2_test/xtc/data-amox27716-r0100-acqiris-e001000.xtc2')
+    DETNAME      = kwargs.get('detname','tmo_quadanode')
+    EVSKIP       = kwargs.get('evskip', 0)
+    EVENTS       = kwargs.get('events', 10) + EVSKIP
+    EXP          = kwargs.get('exp', 'amox27716')
+    RUN          = kwargs.get('run', 100)
+    VERBOSE      = kwargs.get('verbose', True)
+    OFPREFIX     = kwargs.get('ofprefix','./')
+
+    peaks = WFPeaks(**kwargs)
+
+    ds    = DataSource(files=DSNAME)
+    orun  = next(ds.runs())
+    det   = orun.Detector(DETNAME)
+
+    tb_sec = time()
+    nev = 0
+    for nev,evt in enumerate(orun.events()):
+
+        if nev<EVSKIP: continue
+        if nev>EVENTS: break
+
+        if do_print(nev): logger.info('Event %4d'%nev)
+        t0_sec = time()
+
+        wts = det.raw.times(evt)
+        wfs = det.raw.waveforms(evt)
+
+        nhits, pkinds, pkvals, pktsec = peaks(wfs,wts) # ACCESS TO PEAK INFO
+
+        # Function peaks returns `pktsec` for each channel. We need to locate
+        # index of these peaks in wts. The indices will be used to identify
+        # windows of waveform wfs and startpos in wts.
+        n_chans = len(nhits)
+        window_size = 8
+        print(f'nev={nev}')
+        for i_chan in range(n_chans):
+            print(f'  i_chan={i_chan}/{n_chans}')
+            print(f'  nhits={nhits[i_chan]}')
+            print(f'  pktsec={pktsec[i_chan]}')
+            peak_ind = np.searchsorted(wts[i_chan,:], pktsec[i_chan][:nhits[i_chan]])
+
+            # Find peak windows
+            peaks_arr, startpos_arr = get_window_from_peaks(
+                    wfs[i_chan,:], wts[i_chan,:], peak_ind, window_size)
+            
+            n_peaks = nhits[i_chan]
+            for i_peak in range(n_peaks):
+                print(f'    i_peak={i_peak}')
+                print(f'    peaks={peaks_arr[i_peak,:]}')
+                print(f'    startpos={startpos_arr[i_peak]}')
+
+        if VERBOSE:
+            print("  ev:%4d waveforms processing time = %.6f sec" % (nev, time()-t0_sec))
+            print_ndarr(wfs,    '    waveforms      : ', last=4)
+            print_ndarr(wts,    '    times          : ', last=4)
+            print_ndarr(nhits,  '    number_of_hits : ')
+            print_ndarr(pktsec, '    peak_times_sec : ', last=4)
+
+
+    print("  ev:%4d processing time = %.6f sec" % (nev, time()-tb_sec))
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', datefmt='%Y-%m-%dT%H:%M:%S', level=logging.INFO)
+
+    tname = sys.argv[1] if len(sys.argv) > 1 else '1'
+    print('%s\nTEST %s' % (50*'_', tname))
+
+    kwargs = {'dsname'   : '/reg/g/psdm/detector/data2_test/xtc/data-amox27716-r0100-acqiris-e001000.xtc2',
+              'detname'  : 'tmo_quadanode',
+              'numchs'   : 5,
+              'numhits'  : 16,
+              'evskip'   : 0,
+              'events'   : 1,
+              'ofprefix' : './',
+              'run'      : 100,
+              'exp'      : 'amox27716',
+              'version'  : 4,
+              'DLD'      : True,
+              'paramsCFD': {0: {'channel': 'mcp',
+                              'delay': 3.068e-09,
+                              'fraction': 0.35,
+                              'offset': 0.054470544805354439,
+                              'polarity': 'Negative',
+                              'sample_interval': 2.5e-10,
+                              'threshold': 0.056374120466532174,
+                              'timerange_high': 1e-05,
+                              'timerange_low': 1e-06,
+                              'walk': 0},
+                              1: {'channel': 'x1',
+                              'delay': 3.997500000000001e-09,
+                              'fraction': 0.35,
+                              'offset': 0.032654320557811034,
+                              'polarity': 'Negative',
+                              'sample_interval': 2.5e-10,
+                              'threshold': 0.048439800379417808,
+                              'timerange_high': 1e-05,
+                              'timerange_low': 1e-06,
+                              'walk': 0},
+                             2: {'channel': 'x2',
+                               'delay': 4.712500000000001e-09,
+                              'fraction': 0.35,
+                              'offset': 0.058295909692775157,
+                              'polarity': 'Negative',
+                              'sample_interval': 2.5e-10,
+                              'threshold': 0.062173077232695384,
+                              'timerange_high': 1e-05,
+                              'timerange_low': 1e-06,
+                              'walk': 0},
+                             3: {'channel': 'y1',
+                              'delay': 4.5435e-09,
+                              'fraction': 0.35,
+                              'offset': 0.01740340726630819,
+                              'polarity': 'Negative',
+                              'sample_interval': 2.5e-10,
+                              'threshold': 0.035850750860370109,
+                              'timerange_high': 1e-05,
+                              'timerange_low': 1e-06,
+                              'walk': 0},
+                             4: {'channel': 'y2',
+                              'delay': 4.140500000000001e-09,
+                              'fraction': 0.35,
+                              'offset': 0.0088379291811293368,
+                              'polarity': 'Negative',
+                              'sample_interval': 2.5e-10,
+                              'threshold': 0.035254198205580331,
+                              'timerange_high': 1e-05,
+                              'timerange_low': 1e-06,
+                              'walk': 0}}}
+    # Parameters of the CFD descriminator for hit time finding algotithm
+    cfdpars= {'cfd_base'       :  0.,
+              'cfd_thr'        : -0.05,
+              'cfd_cfr'        :  0.85,
+              'cfd_deadtime'   :  10.0,
+              'cfd_leadingedge':  True,
+              'cfd_ioffsetbeg' :  1000,
+              'cfd_ioffsetend' :  2000,
+              'cfd_wfbinbeg'   :  6000,
+              'cfd_wfbinend'   : 22000,
+             }
+
+    kwargs.update(cfdpars)
+
+    proc_data(**kwargs)
+
+    print('\n%s' % USAGE)
+    sys.exit('End of %s' % sys.argv[0])
+
+# EOF
