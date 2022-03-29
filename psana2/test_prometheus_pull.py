f77aff2d6cdbf5f9ec4e4ff6387c64f9ab363232
monarin <monarin@gmail.com>
2022-03-29 13:08:49 -0700

    add simple prometheus push and pull test files

---
+++ psana2/test_prometheus_pull.py
@@ -0,0 +1,60 @@
+
+import os
+import time
+import jmespath
+from datetime import datetime, timedelta
+import dmmon.promquery as pq
+import numpy as np
+import sys, os
+
+# Disable print
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore print
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+class MetricDisplay(object):
+    def __init__(self, srvurl, start, stop, jobid):
+        self.start  = start
+        self.stop   = stop
+        self.srvurl = srvurl
+        self.jobid  = jobid
+
+    def get_rate(self, metric_name):
+        blockPrint()        # block promequery print
+        data = pq.get_data_prom(self.srvurl,
+                'rate(%s{jobid="%s"}[15s])'%(metric_name, self.jobid),
+                self.start.timestamp(), self.stop.timestamp(), step='5s')
+        enablePrint()
+        return data
+
+
+def main(srvurl, jobid, start=None, step_seconds=15):
+    
+    if not start:
+        # Default is the last step_seconds
+        stop = datetime.now()
+        start = stop - timedelta(seconds=step_seconds) 
+    else:
+        # Or query from start with query window = step_seconds 
+        stop = start + timedelta(seconds=step_seconds)
+
+    print(f"QUERY FROM {start} TO {stop} ")
+    print(f"STEP={step_seconds}s")
+
+    md = MetricDisplay(srvurl, start, stop, jobid)
+    data = md.get_rate("psana_bd_wait_eb_total")
+    print(data)
+
+
+if __name__ == "__main__":
+    jobid = sys.argv[1]
+    start = None
+    if len(sys.argv) > 2:
+        start = datetime.fromtimestamp(int(sys.argv[2]))
+    srvurl = os.environ.get("DM_PROM_SERVER", "http://psmetric03:9090") 
+
+    print("Using server", srvurl, " jobid:", jobid)
+    main(srvurl, jobid, start=start)
