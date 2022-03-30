1369c8e0d6d023c9011ea609575ed4dcb12bdc08
monarin <monarin@gmail.com>
2022-03-30 11:21:53 -0700

    make better printing

--- psana2/test_live.py
+++ psana2/test_live.py
@@ -1,3 +1,9 @@
+""" For testing live mode and other PSANA2 performance measurement
+
+on drp-srcf, generate slurm_hosts then run
+SLURM_HOSTFILE=slurm_hosts srun -o log.log --partition=anaq --exclusive python -u test_live.py
+
+"""
 import time
 import os
 from psana import DataSource
@@ -16,18 +22,25 @@ rank = comm.Get_rank()
 #formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 #ch.setFormatter(formatter)
 #logger.addHandler(ch)
+
 def test_standard():
-    #exp='tstx00417'
-    #runno=224
-    #xtc_dir='/cds/data/drpsrcf/tst/tstx00417/xtc'
-    exp='rixtst099'
-    runno=12
-    xtc_dir='/cds/data/drpsrcf/rix/rixtst099/xtc/'
     batch_size = 1000
-    max_events = 0
-    comm.Barrier()
-    t0 = MPI.Wtime()
-    ds = DataSource(exp=exp, run=runno, batch_size=batch_size, max_events=max_events, dir=xtc_dir, live=True)
+    max_events = int(10e6)
+    
+    exp='tstx00417'
+    runno=224
+    xtc_dir='/cds/data/drpsrcf/tst/tstx00417/xtc'
+
+    #exp='rixtst099'
+    #runno=12
+    #xtc_dir='/cds/data/drpsrcf/rix/rixtst099/xtc/'
+
+    ds = DataSource(exp=exp, 
+                    run=runno, 
+                    batch_size=batch_size, 
+                    max_events=max_events, 
+                    dir=xtc_dir, 
+                    live=True)
 
     sendbuf = np.zeros(1, dtype='i')
     recvbuf = None
@@ -37,18 +50,31 @@ def test_standard():
     st = time.time()
     for run in ds.runs():
         for nevt, evt in enumerate(run.events()):
-            if nevt % 1000 == 0:
+            if nevt % 1000 == 0 and nevt > 0:
                 en = time.time()
-                print(nevt, evt.timestamp, (1000/(en-st))*1e-3, flush=True)
+                print(f'RANK: {rank:4d} EVENTS: {nevt:10d} RATE: {(1000/(en-st))*1e-3:.2f}kHz', flush=True)
                 st = time.time()
             sendbuf += 1
-
+    
+    # Count total no. of events
     comm.Gather(sendbuf, recvbuf, root=0)
+    if rank == 0:
+        n_events = np.sum(recvbuf)
+    else:
+        n_events = None
+    n_events = comm.bcast(n_events, root=0)
+    return n_events
+
+
+if __name__ == "__main__":
+    comm.Barrier()
+    t0 = MPI.Wtime()
+    
+    n_events = test_standard()
+    
     comm.Barrier()
     t1 = MPI.Wtime()
     if rank == 0:
         n_eb_nodes = int(os.environ.get('PS_EB_NODES', '1'))
-        print(f'TOTAL TIME:{t1-t0:.2f}s #EB: {n_eb_nodes} n_events={np.sum(recvbuf)}')
-
-test_standard()
+        print(f'TOTAL TIME:{t1-t0:.2f}s #EB: {n_eb_nodes:3d} EVENTS:{n_events:10d} RATE:{(n_events/(t1-t0))*1e-6:.2f}MHz', flush=True)
 
