86a4eab30fbec70447e0c1b635b119ce129bb220
Monarin Uervirojnangkoorn <monarin@gmail.com>
2023-09-27 14:27:23 -0700

    set PS_SMD_N_EVENTS to process integrating events correctly

--- psana2/test_intg_det.py
+++ psana2/test_intg_det.py
@@ -1,28 +1,29 @@
 import numpy as np
+import os
 # This `batch_size` should be set to a small number (e.g. 1)
 # since all other events which are part of this intg. event will be sent
 # in the same batch.
-batch_size = 1
+os.environ['PS_SMD_N_EVENTS']='1'
  
 from psana import DataSource
-ds = DataSource(exp='xpptut15', run=1, dir='/cds/data/psdm/prj/public01/xtc/intg_det',
-        intg_det='andor',        
-        batch_size=batch_size)
+ds = DataSource(exp='rixx1003721', run=171, 
+        intg_det='andor_vls',)
 run = next(ds.runs())
 hsd = run.Detector('hsd')
-andor = run.Detector('andor')
+andor = run.Detector('andor_vls')
  
 # Test calculating sum of the hsd for each integrating event.
 sum_hsd = 0
 for i_evt, evt in enumerate(run.events()):
-    hsd_calib = hsd.raw.calib(evt)
-    andor_calib = andor.raw.calib(evt)
+    hsd_peaks = hsd.raw.waveforms(evt)[0][0]
+    if hsd_peaks is None: continue
+    andor_val = andor.raw.value(evt)
  
     # Keep summing the value of the other detector (hsd in this case)
-    sum_hsd += np.sum(hsd_calib[:])/np.prod(hsd_calib.shape)
+    sum_hsd += np.sum(hsd_peaks[:])/np.prod(hsd_peaks.shape)
      
     # When an integrating event is found, print out and reset the sum variable
-    if andor_calib is not None:
-        val_andor = np.sum(andor_calib[:])/np.prod(andor_calib.shape)
+    if andor_val is not None:
+        val_andor = np.sum(andor_val[:])/np.prod(andor_val.shape)
         print(f'i_evt: {i_evt} andor: {val_andor} sum_hsd:{sum_hsd}')
         sum_hsd = 0
